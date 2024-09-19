3c83f5986eadb8b6bdccb48edfdcd8605549c827
Олег Призов <oleg12345prisov@gmail.com>
2024-09-19 21:49:18 +0300

    v1.1

--- backend/sites/models.py
+++ backend/sites/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from users.models import User
- 
+
 class Site(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
